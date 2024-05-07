090a79fa8c7cfdc32a0bfcbc6a49007ee6949a61
iagokenay <113652236+iagokenay@users.noreply.github.com>
2024-05-06 21:41:17 -0300

    Add files via upload

---
+++ WebBack-End/aula02-03-04/aula04.js
@@ -0,0 +1,21 @@
+// o http define varios metodos que indicam a ação a ser realizada 
+
+
+// GET - PARA RECEBER DADOS 
+
+// POST - PARA ENVIAR DADOS 
+
+// PUT -  PARA ATUALIZAR DADOS 
+
+// DELETE - PARA EXCLUIR DADOS 
+
+//\\//\\//\//\/\//\/\/\/\/\///\\//\\///\
+
+// OS CABEÇARIOS PODEM ESPECIFICAR : HTTTP
+
+// O TIPO DE CONTEUDO
+// A CODIFICAÇÃO
+// COOKIES
+// AUTENTIFICAÇÃO
+
+// O HTTP E  "STATELESS"
