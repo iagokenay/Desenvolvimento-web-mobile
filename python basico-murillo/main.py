28ccc1a3d8e05e940f78c5e351c8d74e32224e1f
iagokenay <113652236+iagokenay@users.noreply.github.com>
2024-05-14 19:59:00 -0300

    Add files via upload

---
+++ python basico-murillo/main.py
@@ -0,0 +1,19 @@
+import pygame
+
+import pygame
+
+#inicializando modulos de pygame
+pygame.init()
+#criando uma janela com o titulo "ola, mundo"
+janela = pygame.display.set_mode((400,300))
+pygame.display.set_caption("ola mundo")
+
+deve_continuar = True
+#loop do jogo
+while deve_continuar:
+    #checando eventos
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            deve_continuar = False
+#encerrendo o modolos de pygame
+pygame.quit()
