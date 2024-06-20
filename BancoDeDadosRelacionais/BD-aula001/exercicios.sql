98759e871213048faa147922b42e0f6348c3d8f0
iagokenay <113652236+iagokenay@users.noreply.github.com>
2024-06-19 22:46:06 -0300

    Add files via upload

---
+++ BancoDeDadosRelacionais/BD-aula001/exercicios.sql
@@ -0,0 +1,11 @@
+PROBLEMA:2602 - Select Básico
+LINGUAGEM:PostgreSQL (psql 9.4.19)
+
+SELECT name
+FROM customers WHERE state = 'RS'
+
+PROBLEMA: 2603 - Endereço dos Clientes
+LINGUAGEM: PostgreSQL (psql 9.4.19)
+
+SELECT name,street
+FROM customers WHERE city = 'Porto Alegre'
